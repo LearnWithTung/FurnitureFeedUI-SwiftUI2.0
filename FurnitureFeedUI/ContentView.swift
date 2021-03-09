7a16b44390b4d1d39e53ca0099c3dada5af649e4
learnwithtung <tungvuduc2805.apple@gmail.com>
2021-03-09 16:01:13 +0700

    init project

---
+++ FurnitureFeedUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FurnitureFeedUI
+//
+//  Created by Tung Vu Duc on 09/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FurnitureFeedUI/FurnitureFeedUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureFeedUIApp.swift
+//  FurnitureFeedUI
+//
+//  Created by Tung Vu Duc on 09/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureFeedUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
